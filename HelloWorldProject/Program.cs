1f65ae6c9db62a14551167aa35016b683199e2c7
max-student <max-student@maxtrain.com>
2020-02-10 09:12:31 -0500

    First C# Assignemnt

---
+++ HelloWorldProject/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace HelloWorldProject {
+    class Program {
+        static void Main(string[] args) {
+
+            var s1 = new Student();
+            s1.Name = "Fred";
+            s1.FavoriteColor = "Blue";
+            s1.FavoriteNumber = 88;
+            s1.ToConsole();
+
+            var s2 = new Student();
+            s2.Name = "Betty";
+            s2.FavoriteColor = "Green";
+            s2.FavoriteNumber = 27;
+            s2.ToConsole();
+
+            Console.WriteLine("This is my Hello World C# Program");
+            Console.WriteLine("This is another line.");
+
+            var counter = 1; //to comment one line
+            if (counter == 0) {
+                Console.WriteLine("Counter is 0");
+                }
+            else {
+                Console.WriteLine("Counter is not zero");
+                }
+            counter += 1;
+            Console.WriteLine("Counter value is {0}", counter);
+
+            counter++; //same thing as counter = counter + 1, counter += 1
+            Console.WriteLine($"Count value is {counter}");
+
+            var sum = 0;
+            for (var index = 0; index <= 1000; index++) {
+                sum += index;
+                }
+            Console.WriteLine($"Sum is {sum}");
+            }
+        }
+    }
---
+++ HelloWorldProject/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloWorldProject {
+    class Student {
+        public string FavoriteColor; //Property in a class, does not use var use type
+        public int FavoriteNumber;
+        public string Name;
+
+        public void ToConsole() {
+            Console.WriteLine($"Name is {Name}, Color is {FavoriteColor}, Number is {FavoriteNumber}");
+            }
+        }
+    }
